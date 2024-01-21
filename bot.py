bf8c879e57271efbbc17b3a3eeb60dc43b0a51a9
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-01-21 16:39:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,4 +6,22 @@ from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader 
 from datetime import datetime
 
-print(ALPACA_CREDS)
+
+class MLTrader(Strategy):
+    def initialize(self, symbol:str="SPY"):
+        self.symbol = symbol
+        pass
+    def on_trading_iteration(self):
+        pass
+
+start_date = datetime(2023, 12, 15)
+end_date = datetime(2023, 12, 31)
+
+broker = Alpaca(ALPACA_CREDS)
+strategy = MLTrader(name='mlstrat', broker=broker,  parameters={"symbol":"SPY"})
+strategy.backtest(
+    YahooDataBacktesting,
+    start_date,
+    end_date,
+    parameters={}
+)
