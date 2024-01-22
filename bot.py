7dadac54735ac08d6d6cf4b263f7ee7a82b5f1f7
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-01-22 23:29:30 +0530

    update bot

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from lumibot.strategies import Strategy
 from lumibot.traders import Trader
 
 class TradeStrategy(Strategy):
-    def initialize(self, symbol:str="UNH", cash_at_risk:float=.5):
+    def initialize(self, symbol:str="SPY", cash_at_risk:float=.5):
         self.symbol = symbol
         self.cash_at_risk = cash_at_risk
         self.sleeptime = "24H"
@@ -14,23 +14,30 @@ class TradeStrategy(Strategy):
     def position_sizing(self):
         cash = self.get_cash()
         last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price)
+        return cash, last_price, quantity
         
     def on_trading_iteration(self):
-        if self.last_trade==None:
-            order = self.create_order(
-                self.symbol,
-                10,
-                "buy",
-                type="market"
-            )
-            self.submit_order(order)
-            self.last_trade="buy"
+        cash, last_price, quantity = self.position_sizing()
+
+        if cash > last_price:
+            if self.last_trade==None:
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type="bracket",
+                    take_profit_price=last_price*1.20,
+                    stop_loss_price=last_price*.95
+                )
+                self.submit_order(order)
+                self.last_trade="buy"
 
 
 # Pick the dates that you want to start and end your backtest
 # and the allocated budget
-backtesting_start = datetime(2020, 12, 12)
-backtesting_end = datetime(2020, 12, 21)
+backtesting_start = datetime(2020, 12, 1)
+backtesting_end = datetime(2020, 12, 31)
 
 # Run the backtest
 trader = Trader(backtest=True)
