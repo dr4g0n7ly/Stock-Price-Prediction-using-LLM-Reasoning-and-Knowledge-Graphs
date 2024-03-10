1d4dae6df0b86c965ab3cb1a2bd5f1bd8ef2e612
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-03-10 16:06:48 +0530

    Update

--- REST_news_bot.py
+++ REST_news_bot.py
@@ -3,6 +3,7 @@ from config import BASE_URL, API_KEY, API_SECRET
 
 api = tradeapi.REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 news = api.get_news(symbol='TSLA', start="2023-06-08",end="2023-06-09", limit=10)
+news = [ev.__dict__["_raw"]["headline"] for ev in news]
 
 print("News for TSLA:")
 print(news)
