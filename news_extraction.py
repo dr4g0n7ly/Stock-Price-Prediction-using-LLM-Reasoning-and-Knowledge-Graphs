a9b30e98c055771ddcfb977a5ee95e2fb4c35414
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-03-13 22:41:47 +0530

    news extraction

---
+++ news_extraction.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from marketaux_news import get_news
+from datetime import datetime, timedelta 
+from config import MARKETAUX_KEY
+
+api_token = MARKETAUX_KEY
+symbols = 'TSLA'
+start_date = datetime.strptime('2024-01-02', '%Y-%m-%d')
+num_days = 2
+
+try:
+    df = pd.read_csv('tesla_news.csv')
+except FileNotFoundError:
+    df = pd.DataFrame(columns=['date', 'news'])
+
+for i in range(0, num_days):
+    publish_date = (start_date + timedelta(days=i)).strftime('%Y-%m-%d')
+    summary = get_news(api_token, symbols, publish_date)
+    new_df = pd.DataFrame({"date": [publish_date], "news": [summary]})
+    df = pd.concat([df, new_df], ignore_index=True)
+    df.to_csv('tesla_news.csv', index=False)
+
+
