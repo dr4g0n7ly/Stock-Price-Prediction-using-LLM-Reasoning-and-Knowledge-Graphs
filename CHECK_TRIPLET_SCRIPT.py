0583a5987ea75204df3cde7685ccbc3631e9b798
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-04-29 21:40:55 +0530

    updated triplets

--- CHECK_TRIPLET_SCRIPT.py
+++ CHECK_TRIPLET_SCRIPT.py
@@ -1,33 +1,32 @@
-# Read the CSV file
-
+# CHANGE FILENAME TO LLM REASON OUTPUT FILE
+FILENAME = 'tesla_news'
 
 import pandas as pd
-import json
-
-FILENAME = "tesla_news"
-
-df = pd.read_csv(FILENAME+'_with_triplets.csv')
-
-# df['LLM_Triplet'] = df['LLM_Triplet'].str.replace("```", '')
-# df.to_csv('modified_tesla_news_with_triplets.csv', index=False)
 import json
 
 def check_json_format(df):
     for index, row in df.iterrows():
         try:
-            triplet_json = json.loads(row['LLM_Triplet'])
-            # if 'triplets' not in triplet_json and 'error' not in triplet_json:
-            #     print(f"Row {index + 1}: LLM_Triplet doesn't contain 'triplets' or 'error'.")
-            # elif 'triplets' in triplet_json:
-            #     triplets = triplet_json['triplets']
-            #     for triplet in triplets:
-            #         if 'head' not in triplet or 'relation' not in triplet or 'tail' not in triplet:
-            #             print(f"Row {index + 1}: One or more elements in 'triplets' are missing required keys.")
+            output = json.loads(row['LLM_Triplet'])
+            if 'triplets' not in output and 'error' not in output:
+                print(f"Row {index + 1} Date - {row['date']}: LLM_Triplet doesn't contain 'confidence' or 'error'.")
         except json.JSONDecodeError:
             print(f"Date - {row['date']}: LLM_Triplet is not in valid JSON format.")
 
-# Example usage:
-# check_json_format(your_dataframe)
 
 # Call the function to check JSON format
+df = pd.read_csv(FILENAME+'_with_triplets.csv')
+
+df['LLM_Triplet'] = df['LLM_Triplet'].str.replace("```", '')
+df['LLM_Triplet'] = df['LLM_Triplet'].str.replace("json", '')
+df['LLM_Triplet'] = df['LLM_Triplet'].str.replace("JSON", '')
+df['LLM_Triplet'] = df['LLM_Triplet'].str.replace("/", "'")
+df['LLM_Triplet'] = df['LLM_Triplet'].str.replace("_", ' ')
+df['LLM_Triplet'] = df['LLM_Triplet'].str.replace("error: news blocked", '"error": "news blocked"')
+
+# df.drop(columns=['news'], inplace=True)
+
+# df.to_csv(FILENAME+'_with_triplets.csv', index=False)
+
+
 check_json_format(df)
