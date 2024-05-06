ee3afa6cd802a9d314b97d3654c2a3cd7a66686b
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-05-06 15:47:46 +0530

    KG confidence extraction + tesla sentiment

---
+++ CONFIDENCE_EXTRACTION_KG.py
@@ -0,0 +1,37 @@
+# CHANGE FILENAME TO LLM REASON OUTPUT FILE
+FILENAME = 'tesla_news'
+
+import pandas as pd
+import json
+
+def check_json_format(df):
+    for index, row in df.iterrows():
+        try:
+            if pd.isna(row['LLM_output']):
+                df.at[index, 'confidence'] = 0
+                continue
+
+            output = json.loads(row['LLM_output'])
+            if 'confidence' in output:
+                df.at[index, 'confidence'] = output['confidence']
+            else:
+                print(f"Row {index + 1}: LLM_output doesn't contain 'confidence'.")
+            if 'reason' not in output and 'error' not in output:
+                print(f"Row {index + 1}: LLM_output doesn't contain 'reason' or 'error'.")
+        except json.JSONDecodeError:
+            print(f"Date - {row['date']}: LLM_output is not in valid JSON format.")
+
+    df.to_csv(FILENAME + '_KG_confidence.csv', index=False)
+
+
+
+# Call the function to check JSON format
+df = pd.read_csv(FILENAME+'_with_LLM_KG.csv')
+
+df['LLM_output'] = df['LLM_output'].str.replace("```", '')
+df['LLM_output'] = df['LLM_output'].str.replace("json", '')
+df['LLM_output'] = df['LLM_output'].str.replace("JSON", '')
+df['LLM_output'] = df['LLM_output'].str.replace("reason: error, confidence: 2", '""reason"": ""error"", ""confidence"": 2')
+
+
+check_json_format(df)
